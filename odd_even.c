c192203290cf2aed9dd3be2d68603a286f0b0cfe
acherikx <amarnath.3297@gmail.com>
2018-05-07 11:14:30 +0530

    Update odd_even.c
    Updated var names...!

--- odd_even.c
+++ odd_even.c
@@ -6,8 +6,8 @@ void main()
  
     printf("Enter an integer : ");
     scanf("%d", &ival);
-    remainder = !(ival & 1);
-    if (remainder == 0)
+    rem = !(ival & 1);
+    if (rem == 0)
         printf("%d is an even integer\n", ival);
     else
         printf("%d is an odd integer\n", ival);
